2136178a68342fb196b28a849b42e5975371c92e
haarismian <mail@haarismian.com>
2023-09-15 16:20:52 -0400

    comments

--- app.py
+++ app.py
@@ -3,15 +3,13 @@ This script visualizes a hierarchical tree structure described in a JSON file.
 It uses matplotlib to visualize the tree and allows interactive updating of node values.
 """
 
-# Import libraries
 import json  # To work with JSON data
 import matplotlib.pyplot as plt  # To visualize data using various plot types
-import networkx as nx  # To create, manipulate, and study complex networks of nodes and edges
+import networkx as nx  # To create and edit networks of nodes and edges
 from networkx.drawing.nx_agraph import graphviz_layout  # To use Graphviz for better layout and visualization of the graph
 
 
 
-# Recursive function to add edges and nodes to the graph from the JSON data
 def add_edges(graph, node, parent=None):
     """
     Recursive function to add edges and nodes to the graph from the JSON data.
@@ -22,16 +20,14 @@ def add_edges(graph, node, parent=None):
     parent (str, optional): The parent of the current node. Defaults to None.
     """
    
-    # For the first call (starting with the root node), parent is None. This is when the root node is added to the graph, but no edge is added because parent is None.
+    # For the first call (starting with the root node), parent is None. This is when the root node is added to the graph.
     if parent is not None:
         graph.add_edge(parent, node['id'])
     # Each node from the JSON data is added to the graph including its predefined id, label, and value
     graph.add_node(node['id'], label=node['label'], value=node.get('value', 0))
-    # If the node has children, add edges (relationships between nodes) and nodes recursively, making the current node the parent
+    # If the node in the json has children, add edges (relationships between nodes) and nodes recursively, making the current node the parent
     for child in node.get('children', []):
         add_edges(graph, child, node['id'])
-
-# Function to find and sum the values of all the ancestors of a node
 
 
 def find_parent_values(graph, node):
@@ -46,7 +42,7 @@ def find_parent_values(graph, node):
     float: The sum of the values of all ancestor nodes.
     """
     parents = nx.ancestors(graph, node)
-    # for each of the parent nodes of the selected node, find and sum all of the values
+    # When a node is selected, the user might want to see the sum of all the ancestors for that node
     values = [graph.nodes[parent].get('value', 0) for parent in parents]
     return sum(values)
 
@@ -62,26 +58,26 @@ def visualize_attack_tree(json_graph_data):
     """
     graph = nx.DiGraph()  # Create a directed graph, the shape of an attack tree
 
-    # Add nodes using recursive function
+    # Add nodes to the visualization recursively, as it would be the most efficient way to go through the nested JSON
     add_edges(graph, json_graph_data)
 
-    # Set up graph layout using graphviz dot layout https://graphviz.org/docs/layouts/dot/
+    # Set up graph layout using graphviz dot layout, which is the closest one to an attack tree https://graphviz.org/docs/layouts/dot/
     pos = graphviz_layout(graph, prog="dot")
 
-    # Create dictionary of labels and node values for the graph
+    # Create dictionary of labels and node values for the graph, this is necessary to update the labels as Matplotlib does not support dynamic labels
     labels = {
         node: f"{graph.nodes[node]['label']} ({graph.nodes[node].get('value', '')})" for node in graph.nodes()}
 
-    # Create dictionary of nodes and their respective indices for updating later
+    # Create dictionary of nodes and their respective indices, as this will be used as a lookup tool later for updating the JSON data, which will be used to redraw the graph
     index_to_node = {i: node for i, node in enumerate(graph.nodes)}
 
     # Event handler function for clicking on nodes
     def on_pick(event):
-        # Get node attributes and index when it is clicked so we can update the value
+        # Get node attributes and index when it is clicked, this will be used to update the JSON data
         node_idx = index_to_node[event.ind[0]]
         node_attrs = graph.nodes[node_idx]
 
-        # Find and display the sum of all the parent node values
+        # Find and display the sum of all the parent node values of a selected node
         parent_sum = find_parent_values(graph, node_idx)
 
         # Get new value input from the user
