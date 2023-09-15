5a0c7f805fa0c1202bb8ee45cca1c9c9af6fa1d9
haarismian <mail@haarismian.com>
2023-09-15 02:11:03 -0400

    font size

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def draw_graph(graph, pos, labels):
     # Graph nodes are drawn  followed by labels
     nx.draw(graph, pos,
             node_size=500, node_color='skyblue')
-    nx.draw_networkx_labels(graph, pos, labels=labels, font_size=12)
+    nx.draw_networkx_labels(graph, pos, labels=labels)
 
 
 # Function to sum individual paths recursively to show user in the following format:
@@ -199,8 +199,8 @@ def sum_routes(json_graph):
 if __name__ == "__main__":
     # Specify the JSON file path
     # filename = "./digitization_challenges.json"
-    filename = "./local_supplier_issues.json"
-    # filename = "./data_breach.json"
+    # filename = "./local_supplier_issues.json"
+    filename = "./data_breach.json"
 
     # Execute both sum_routes and visualize_attack_tree functions
 
